330b077bf0c5eb5340784ccb7b6b89ddcdf89ead
petchzaloveyou <petchloveyou@gmail.com>
2015-12-08 14:54:20 +0700

    Update finding.py

--- finding.py
+++ finding.py
@@ -1,1 +1,7 @@
-
+class finding(objrct):
+  
+  def __init__(self, latitude,longitude):
+		self.latitude = latitude
+		self.longitude = longitude
+	def position(self):
+		return locate(latitude,longitude)
